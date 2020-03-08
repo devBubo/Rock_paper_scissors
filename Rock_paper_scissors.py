65f0b83448f6464c095ae54c04c1034a930e2fb5
devBubo <bubak.filip@gmail.com>
2020-03-09 00:42:40 +0100

    right hand

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -4,11 +4,13 @@ root.title('Rock Paper Scissors')
 canvas=Canvas()
 canvas.pack()
 canvas.config(width=800,height=600)
-rock=PhotoImage(file='left_fist.png')
+left_hand=PhotoImage(file='left_fist.png')
+right_hand=PhotoImage(file='right_fist.png')
 height=100
 vel=1
 while True:
-    left_fist=canvas.create_image(0, height, anchor=NW, image=rock)
+    left_fist=canvas.create_image(0, height, anchor=NW, image=left_hand)
+    right_fist=canvas.create_image(452,height,anchor=NW,image=right_hand)
     height+=vel
     canvas.update()
     canvas.after(1)
