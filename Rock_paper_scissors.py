6ebfc2e8945924a59858453d10f8a62e4a76acb5
devBubo <56486145+devBubo@users.noreply.github.com>
2020-03-08 23:46:07 +0100

    Update Rock_paper_scissors.py

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,1 +1,19 @@
-
+from tkinter import *
+root = Tk()
+root.title('Rock Paper Scissors')
+canvas=Canvas()
+canvas.pack()
+canvas.config(width=800,height=600)
+rock=PhotoImage(file='left_fist.png')
+height=100
+vel=1
+while True:
+    left_fist=canvas.create_image(0, height, anchor=NW, image=rock)
+    height+=vel
+    canvas.update()
+    canvas.after(1)
+    canvas.delete('all')
+    if height==200:
+        vel*=-1
+    elif height==50:
+        vel*=-1
